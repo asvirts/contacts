419b00d12f5a7690277ab425f42134a6e4aaeb83
asvirts <asvirts@outlook.com>
2024-08-13 20:04:35 -0500

    chore: Add Contact model and views for CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   root 'home#index'
 
   get "up" => "rails/health#show", as: :rails_health_check
---
+++ db/migrate/20240814005518_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :phone
+      t.string :threads
+
+      t.timestamps
+    end
+  end
+end
