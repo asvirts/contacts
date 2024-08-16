f56df00820bdf6db87da7dbe59bc84440d13f0b6
asvirts <asvirts@outlook.com>
2024-08-15 21:20:03 -0500

    Update contact views to include user association and improve form layout

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+# Version: 2024_08_15_013358
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_15_013358) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_16_014200) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -19,6 +9,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_15_013358) do
     t.string "threads"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
